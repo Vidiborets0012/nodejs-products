28f853e771df0b4df92294ba5aa40478bdbc4e13
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-02-07 00:37:13 +0200

    add the Product model

---
+++ src/models/product.js
@@ -0,0 +1,30 @@
+import { model, Schema } from 'mongoose';
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: ['books', 'electronics', 'clothing', 'other'],
+      default: 'other',
+    },
+    description: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const Product = model('Product', productSchema);
