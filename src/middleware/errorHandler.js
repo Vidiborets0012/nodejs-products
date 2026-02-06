0aea7d54922550e3fe383e092ce944768d7aa8ce
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-02-06 23:09:18 +0200

    separate errors for development and for production (isProd)

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,7 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 export const errorHandler = (err, req, res, next) => {
   console.error('Error details:', err);
+
+  const isProd = process.env.NODE_ENV === 'production';
+
   res.status(500).json({
-    message: err.message || 'Internal Server Error',
+    message: isProd
+      ? 'Something went wrong. Please try again later.'
+      : err.message || 'Internal Server Error',
   });
 };
