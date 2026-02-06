9898b48e3dbf51d3b852fa7d4c5a099080bb0136
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-02-06 23:49:05 +0200

    implement connection to the database (MongoDB)

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { logger } from './middleware/logger.js';
 
-// import { connectMongoDB } from './db/connectMongoDB.js';
+import { connectMongoDB } from './db/connectMongoDB.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3030;
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-// await connectMongoDB();
+await connectMongoDB();
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
