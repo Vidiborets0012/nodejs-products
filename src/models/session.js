afaa42633c7f0abd3249caed4b1684dd84295130
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-02-13 19:06:30 +0200

    add Session model

---
+++ src/models/session.js
@@ -0,0 +1,22 @@
+import { model, Schema } from 'mongoose';
+
+const sessionSchema = new Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    accessToken: { type: String, required: true },
+    refreshToken: { type: String, required: true },
+    accessTokenValidUntil: { type: Date, required: true },
+    refreshTokenValidUntil: { type: Date, required: true },
+  },
+  { timestamps: true },
+);
+
+export const Session = model('Session', sessionSchema);
+
+// Створіть модель сесії Session з такими полями:
+
+// userId - string, required
+// accessToken - string, required
+// refreshToken - string, required
+// accessTokenValidUntil - Date, required
+// refreshTokenValidUntil - Date, required
